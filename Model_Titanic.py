cd19d3cbf3a2a36bb9861787c5e07f5d2ea8be2d
malreddy8627 <malreddy8627@gmail.com>
2017-03-06 10:56:02 +0530

    uploading changes

--- Model_Titanic.py
+++ Model_Titanic.py
@@ -50,21 +50,54 @@ titanic_train1.Embarked = le.fit_transform(titanic_train1.Embarked)
 #titanic_train1.Pclass = ohe.fit_transform(titanic_train1.Pclass)
 #titanic_train1.Embarked = ohe.fit_transform(titanic_train1.Embarked)
 
-#x_train = titanic_train1[['Pclass','Sex','Embarked']]
-x_train = titanic_train1[['Fare']]
+x_train = titanic_train1[['Pclass','Sex','Embarked','Fare']]
+#x_train = titanic_train1[['Fare']]
 y_train = titanic_train1['Survived']
 
 dt = tree.DecisionTreeClassifier()
 dt_fit = dt.fit(x_train, y_train)
 
 dot_data = io.StringIO() 
-tree.export_graphviz(dt, out_file = dot_data, feature_names = x.columns)
+tree.export_graphviz(dt, out_file = dot_data, feature_names = x_train.columns)
 graph = pydot.graph_from_dot_data(dot_data.getvalue())[0] 
-graph.write_pdf("dt1.pdf")
+graph.write_pdf("dt2.pdf")
 
+#####reading test data
+titanic_test = pd.read_csv("test.csv")
+titanic_test.shape
+titanic_test.info()
 
+#####handle the missing values 
+###at variable level and at dataframe level
+sum(titanic_test['Age'].isnull()) ####at variable level count
+titanic_test.apply(lambda x : sum(x.isnull()))
+titanic_test.Age[titanic_test['Age'].isnull()] = titanic_test['Age'].mean()
+titanic_test.Fare[titanic_test['Fare'].isnull()] = titanic_test['Fare'].mean()
+###type casting means conversion of object variables to categorical variables.
+###if we look at data type of Sex , as off now it is an object or sometimes it is int also, 
+##it should be converted to categorical variable 
+titanic_test['Sex'] = titanic_test['Sex'].astype('category')
+###we can check with info() method weather sex changed to categorical or not
+titanic_test.info()  
+####like this change all categorical variables
+titanic_test['Pclass'] = titanic_test['Pclass'].astype('category')
+titanic_test['Embarked'] = titanic_test['Embarked'].astype('category')
 
+#####Lebel Encoding , all input categorical variables should be converted to 
+#####numerics coz decision tree algorithm can not understand the categorical variables
 
+titanic_test1 = titanic_test.copy()
+##le = preprocessing.LabelEncoder() ### le is label  encoder already created in train we can use this le
+titanic_test1.Pclass = le.fit_transform(titanic_test1.Pclass)
+titanic_test1.Sex = le.fit_transform(titanic_test1.Sex)
+titanic_test1.Embarked = le.fit_transform(titanic_test1.Embarked)
+
+x_test = titanic_test1[['Pclass','Sex','Embarked','Fare']]
+#x_train = titanic_train1[['Fare']]
+
+titanic_test1['Survived'] = dt.predict(x_test)
+
+titanic_test1.to_csv("submission.csv", columns=['PassengerId','Survived'], index=False)
 
 
 
